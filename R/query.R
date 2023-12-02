e1bc5d42ff2970db90202e938e0d862d2828dd47
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-12-01 20:25:42 -0500

    add query function

---
+++ R/query.R
@@ -0,0 +1,30 @@
+#'
+#' Get Query keywords from a database table.
+#'
+#' @description Description goes here.
+#' @param d the database table.
+#' @param kwds the keywords to look for.
+#' @param column the column to look for the keywords in.
+#' @param ignore_case should the case be ignored when searching for a keyword?
+#' (default TRUE)
+#' @param match_all should we look for values that match all of the keywords
+#' (intersection) or any of the keywords (union)? (default FALSE; union).
+#' @return a query with the keywords to look for.
+#' @importFrom dplyr filter
+#' @export
+
+query_kwds <- function(d, kwds, column, ignore_case = TRUE, match_all = FALSE) {
+  kwds = kwds[kwds != ""]
+  kwds = paste0("%", kwds, "%") |>
+    gsub("'", "''", x = _)
+  if (ignore_case) {
+    like <- " ilike "
+  } else{
+    like <- " like "
+  }
+  query = paste(
+    paste0(column, like, "'", kwds, "'"),
+    collapse = ifelse(match_all, " AND ", " OR ")
+  )
+  filter(d, sql(query))
+}
