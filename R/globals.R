2f84b228d75ab8b6bb06646061aa7ead6e276437
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-12-01 20:55:56 -0500

    add interventions function

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,2 @@
-utils::globalVariables(c("X", "Y", "Z", "value", "Acceleration", "name", "phase"))
+    select(nct_id) |>
+utils::globalVariables(c("X", "Y", "Z", "value", "Acceleration", "name", "phase", "intervention_type", ""))
---
+++ R/intervention.R
@@ -0,0 +1,35 @@
+#' Get interventions on condition mapping
+#' 
+#' @param interventionType the type of interventions.
+#' @return a plot of top 10 conditions on specific intervention type
+#' @importFrom dplyr filter distinct inner_join select count collect slice_max
+#' @importFrom ggplot2 ggplot aes geom_bar coord_flip labs theme_minimal theme element_text
+#' @export
+
+
+
+get_conditions_for_intervention_type <- function(interventionType) {
+
+  intervention_studies <- interventions |>
+    filter(intervention_type == interventionType) |>
+    select(nct_id) |>
+    distinct()
+
+  conditions_for_intervention <- conditions |>
+    inner_join(intervention_studies, by = "nct_id") |>
+    select(name) |>
+    count(name, sort = TRUE) |>
+    collect()
+
+  top_conditions <- conditions_for_intervention %>%
+    slice_max(n, n = 10)
+
+  ggplot(top_conditions, aes(x = reorder(name, n), y = n)) +
+    geom_bar(stat = "identity", fill = 'steelblue') +
+    coord_flip() +
+    labs(y = "Condition", x = "Count",
+         title = paste("Top 10 Conditions for", interventionType)) +
+    theme_minimal() +
+    theme(axis.text.y = element_text(angle = 0))
+
+}
