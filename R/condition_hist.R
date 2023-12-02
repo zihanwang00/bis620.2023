f3e3feba4957387f14071ee0b1ea327eb04ac4c5
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-12-01 20:41:40 -0500

    update condition histogram function

--- R/condition_hist.R
+++ R/condition_hist.R
@@ -3,7 +3,7 @@
 #' @param study the studies to get the number of studies trials for.
 #' @param condition the condition table of the trials.
 #' @return values of top 8 frequent number of conditions
-#' @importFrom dplyr inner_join select group_by summarize arrange head
+#' @importFrom dplyr inner_join select group_by summarize arrange
 #' @export
 
 get_condition_histogram = function(study, condition) {
