0fce338377a84fc016934ef52ca74674f822f19b
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-12-04 00:36:11 -0500

    countries document

--- R/data.R
+++ R/data.R
@@ -9,3 +9,18 @@
 #'   \item{X, Y, Z}{Accelerometry measurement (in milligravities).}
 #' }
 "accel"
+
+#' Country trials data
+#'
+#' A subset of data from the trails and countries
+#'
+#' @format ## `countries`
+#' A data frame with 655,026 rows and 4 columns:
+#' \describe{
+#'   \item{id}{trial id}
+#'   \item{nct_id}{nct_id}
+#'   \item{name}{ciuntry name}
+#'   \item{removed}{trial removed or not}
+#' }
+#'
+"countries"
