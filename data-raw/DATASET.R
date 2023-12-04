8e7c8b7510d89bcddef8be9d0e622b9f81e15845
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-12-04 00:12:37 -0500

    add countries data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
 accel = readRDS("accel.RDS")
+countries = readRDS("filtered_countries.RDS")
 usethis::use_data(accel, overwrite = TRUE)
+usethis::use_data(countries, overwrite = TRUE)
