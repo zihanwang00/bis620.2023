e9b3ea00d33300f35f7ef6268b0bab4d5f48e4ed
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-12-18 18:24:23 -0800

    update dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,14 @@
 ## code to prepare `DATASET` dataset goes here
 
 accel = readRDS("accel.RDS")
+adae = readRDS("adae.RDS")
+adsl = readRDS("adsl.RDS")
+biomark = readRDS("biomark.RDS")
+
 
 usethis::use_data(accel, overwrite = TRUE)
+usethis::use_data(adae, overwrite = TRUE)
+usethis::use_data(adsl, overwrite = TRUE)
+usethis::use_data(biomark, overwrite = TRUE)
+
 
